663787bae5ba1e99b2dda1bc93bbcd0b64736123
geniussjack <150549682+geniussjack@users.noreply.github.com>
2024-05-30 14:08:35 +0500

    Add files via upload

---
+++ CashRegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CashRegisterWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CashRegisterController : ControllerBase
+    {
+        private readonly ICashRegisterDriver _cashRegisterDriver;
+
+        public CashRegisterController(ICashRegisterDriver cashRegisterDriver)
+        {
+            _cashRegisterDriver = cashRegisterDriver;
+        }
+    }
+}
